a75d7b689e2dd47a5d7a0502ba515eafacdcc651
Israel Abraham <israelvictory87@gmail.com>
2022-01-08 23:03:29 +0100

    Users can now signup with referral link and will be added to the recommeded_user

--- ambassadors/users/views.py
+++ ambassadors/users/views.py
@@ -1,3 +1,4 @@
+from django.core.checks import messages
 from django.shortcuts import render
 from django.shortcuts import redirect, render
 from .models import Account
@@ -13,9 +14,11 @@ def signup_view(request):
     
     # Get referral user id
     profile_id = request.session.get('ref_profile')
+    print("Referral ID: ", profile_id)
     
     # Get user by the referral user id
     recommended_by_profile = Account.objects.get(id=profile_id)
+    print("Recommended By Profile: ", recommended_by_profile.fullname())
     
     # User Profile and Account form creation
     userform = UserForm()
@@ -29,14 +32,75 @@ def signup_view(request):
             print('is valid', userform.is_valid())
             
             if profile_id is not None:
-
-                userform.save()
-                instance = userform.save()
-                # form.save()
-                registered_user = User.objects.get(id=instance.id)
+                
+                # Gets cleaned data from user form
+                email = userform.cleaned_data.get("email")
+                password = userform.cleaned_data.get("password")
+                confirm_password = userform.cleaned_data.get("confirm_password")
+                
+                # Checks if password equals to confirm password
+                password = True if password == confirm_password else False
+                
+                # If it does, run the next block of code
+                if password is True:
+                    
+                    # Create user
+                    user = User.objects.create(
+                        email=email,
+                        password=confirm_password
+                    )
+                    
+                    # Set user password
+                    user.password = user.set_password(confirm_password)
+                    
+                else:
+                    
+                    # If it doesn't, redirect the user back to the sign up page
+                    # with an error message telling them that their password
+                    # does not match and should try again!
+                    messages.error(request, "Password does not match. Try again!")
+                    return redirect("users:signup")
+                
+                
+                # Gets user with the instance --> email
+                registered_user = User.objects.get(email=email)
+                print("Registered User: ", registered_user)
+                
+                # Saves form
+                user_account = form.save(commit=False)
+                
+                # Adds user id to user account id
+                user_account.user = registered_user
+                
+                # Saves user_account to database
+                user_account.save()
+                
+                # Gets the registered user of the user account
                 registered_profile = Account.objects.get(user=registered_user)
+                print('Registered Profile: ', registered_profile)
+                
+                # Adds whoever recommended that user to his/her recommended_by
                 registered_profile.recommended_by = recommended_by_profile.user
+                
+                # Saves registered profile to database
                 registered_profile.save()
+                
+                # Authenticate registered user
+                user = authenticate(
+                    username=email, password=confirm_password
+                )
+                
+                # Checks if the user exists
+                if user:
+                    
+                    # Logs the user in
+                    login(registered_user)
+                
+                # Redirects user to the dashboard with a success message
+                messages.success(
+                    request, "Account created successfully!"
+                )
+                return redirect("")
                 
             else:
                 userform.save()
@@ -52,6 +116,7 @@ def signup_view(request):
         'userform': userform,
         'recommended_by_profile': recommended_by_profile
     }
+    print("Recommended By Profile: ", recommended_by_profile.fullname())
     return render(request, 'page/signup.html', context)
 
 
