10dfb611187405e1be63b520862c5f0f64a63cb4
Israel Abraham <israelvictory87@gmail.com>
2022-01-08 23:43:50 +0100

    Created login_page url path

--- ambassadors/users/urls.py
+++ ambassadors/users/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import main_view, signup_view, dashboard
+from .views import (
+    main_view, signup_view, dashboard, login
+)
 
 app_name = "users"
 
 urlpatterns = [
     path('', main_view, name='main-view'),
-    path('signup/', signup_view, name='signup'),
+    path('sign-up/', signup_view, name='signup'),
+    path('sign-in/', login, name="sign-in"),
     path('dashboard/', dashboard, name='recommendations'),
     path('<str:ref_code>/', main_view, name='main-view'),
 ]
