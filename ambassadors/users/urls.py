e926f1bc8a3cda92fd202064af988d7834d69c9f
Israel Abraham <israelvictory87@gmail.com>
2022-01-09 00:13:39 +0100

    Refactor users urls.py

--- ambassadors/users/urls.py
+++ ambassadors/users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import (
-    main_view, signup_view, dashboard, login
+    login_page, main_view, signup_view, dashboard
 )
 
 app_name = "users"
@@ -9,7 +9,7 @@ app_name = "users"
 urlpatterns = [
     path('', main_view, name='main-view'),
     path('sign-up/', signup_view, name='signup'),
-    path('sign-in/', login, name="sign-in"),
+    path('sign-in/', login_page, name="sign-in"),
     path('dashboard/', dashboard, name='recommendations'),
     path('<str:ref_code>/', main_view, name='main-view'),
 ]
